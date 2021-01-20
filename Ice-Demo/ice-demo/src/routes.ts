bcd541cf3410ca7b4dd180bd660285159173c607
eb <a956895479@gmail.com>
2021-01-20 20:02:40 +0800

    update:重制icejs

--- Ice-Demo/ice-demo/src/routes.ts
+++ Ice-Demo/ice-demo/src/routes.ts
@@ -1,27 +1,6 @@
-/*
- * @Description: 
- * @version: 
- * @Author: sdu-gyf
- * @Date: 2021-01-18 18:04:32
- * @LastEditors: sdu-gyf
- * @LastEditTime: 2021-01-19 10:19:54
- */
 import Home from '@/pages/Home';
-import BasicLayout from './layouts/BasicLayout';
-import HttpLearning from './pages/Learning/Http';
 
 const routerConfig = [
-  {
-    path:'/learning',
-    component:BasicLayout,
-    children: [
-      {
-        path:'/http',
-        exact: true,
-        component: HttpLearning
-      }
-    ]
-  },
   {
     path: '/',
     component: Home,
